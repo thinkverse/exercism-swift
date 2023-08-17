de66c9b61972d6662d04ee917b126e8971eb6b8a
Kim Hallberg <hallberg.kim@gmail.com>
2023-08-17 23:35:10 +0200

    Complete `Magician-in-Training` exercise

--- magician-in-training/Sources/MagicianInTraining/MagicianInTraining.swift
+++ magician-in-training/Sources/MagicianInTraining/MagicianInTraining.swift
@@ -1,35 +1,60 @@
+func isInRange(at index: Int, in stack: [Int]) -> Bool {
+  return index >= 0 && index < stack.count
+}
+
 func getCard(at index: Int, from stack: [Int]) -> Int {
-  fatalError("Please implement the getCard(at:from:) function")
+  return isInRange(at: index, in: stack) ? stack[index] : -1
 }
 
 func setCard(at index: Int, in stack: [Int], to newCard: Int) -> [Int] {
-  fatalError("Please implement the setCard(at:from:to:) function")
+  if isInRange(at: index, in: stack) {
+    var newStack = stack
+    newStack[index] = newCard
+
+    return newStack
+  }
+
+  return stack
 }
 
 func insert(_ newCard: Int, atTopOf stack: [Int]) -> [Int] {
-  fatalError("Please implement the insert(_:atTopOf:) function")
+  return insertCard(at: stack.count, from: stack, to: newCard)
 }
 
 func removeCard(at index: Int, from stack: [Int]) -> [Int] {
-  fatalError("Please implement the removeCard(at:from:) function")
+  if isInRange(at: index, in: stack) {
+    var newStack = stack
+    newStack.remove(at: index)
+
+    return newStack
+  }
+
+  return stack
+}
+
+func insertCard(at index: Int, from stack: [Int], to newCard: Int) -> [Int] {
+  var newStack: [Int] = stack
+  newStack.insert(newCard, at: index)
+
+  return newStack
 }
 
 func removeTopCard(_ stack: [Int]) -> [Int] {
-  fatalError("Please implement the removeTopCard(_) function")
+  return removeCard(at: stack.count - 1, from: stack)
 }
 
 func insert(_ newCard: Int, atBottomOf stack: [Int]) -> [Int] {
-  fatalError("Please implement the insert(_:atBottomOf:) function")
+  return insertCard(at: 0, from: stack, to: newCard)
 }
 
 func removeBottomCard(_ stack: [Int]) -> [Int] {
-  fatalError("Please implement the removeBottomCard(_) function")
+  removeCard(at: 0, from: stack)
 }
 
 func checkSizeOfStack(_ stack: [Int], _ size: Int) -> Bool {
-  fatalError("Please implement the checkSizeOfStack(_:_:) function")
+  return stack.count == size
 }
 
 func evenCardCount(_ stack: [Int]) -> Int {
-  fatalError("Please implement the evenCardCount(_) function")
+  return stack.filter{$0.isMultiple(of: 2)}.count
 }
