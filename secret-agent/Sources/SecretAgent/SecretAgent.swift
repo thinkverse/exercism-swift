7253237b82c810968782dc46d617f5e274b7b23b
Kim Hallberg <hallberg.kim@gmail.com>
2023-08-17 18:26:47 +0200

    Add `Secret Agent Double-Null0111` exercise

---
+++ secret-agent/Sources/SecretAgent/SecretAgent.swift
@@ -0,0 +1,7 @@
+func protectSecret(_ secret: String, withPassword password: String) -> (String) -> String {
+  fatalError("Please implement the protectSecret(_:withPassword:) function")
+}
+
+func generateCombination(forRoom room: Int, usingFunction f: (Int) -> Int) -> (Int, Int, Int) {
+  fatalError("Please implement the generateCombination(forRoom:usingFunction) function")
+}
