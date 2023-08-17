3aeb5242aa9773bfdf06f2b05975580702c66b86
Kim Hallberg <hallberg.kim@gmail.com>
2023-08-17 17:32:10 +0200

    Complete `Layers of Lasagna` exercise

--- lasagna/Sources/Lasagna/Lasagna.swift
+++ lasagna/Sources/Lasagna/Lasagna.swift
@@ -1,7 +1,17 @@
 // TODO: define the 'expectedMinutesInOven' constant
+let expectedMinutesInOven = 40
 
 // TODO: define the 'remainingMinutesInOven' function
+func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
+    return expectedMinutesInOven - elapsedMinutes
+}
 
 // TODO: define the 'preparationTimeInMinutes' function
+func preparationTimeInMinutes(layers: Int) -> Int {
+    return layers * 2
+}
 
 // TODO: define the 'totalTimeInMinutes' function
+func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
+    return elapsedMinutes + preparationTimeInMinutes(layers: layers)
+}
