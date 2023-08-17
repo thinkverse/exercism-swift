fee7d0fdd6770edd4e2bea647d8cce6567b1f18d
Kim Hallberg <hallberg.kim@gmail.com>
2023-08-17 22:43:43 +0200

    Complete `Vexing Vehicle Purchase` exercise

--- vehicle-purchase/Sources/VehiclePurchase/VehiclePurchase.swift
+++ vehicle-purchase/Sources/VehiclePurchase/VehiclePurchase.swift
@@ -1,11 +1,34 @@
 func canIBuy(vehicle: String, price: Double, monthlyBudget: Double) -> String {
-  fatalError("Please implement the canIBuy(vehicle:price:monthlyBudget:) function")
+  let totalMonthlyBudget: Double = monthlyBudget * 60
+  let budgetOverCharge: Double = totalMonthlyBudget * 0.10
+
+  if price <= totalMonthlyBudget {
+    return "Yes! I'm getting a \(vehicle)"
+  } else if price <= totalMonthlyBudget + budgetOverCharge {
+    return "I'll have to be frugal if I want a \(vehicle)"
+  } else {
+    return "Darn! No \(vehicle) for me"
+  }
 }
 
 func licenseType(numberOfWheels wheels: Int) -> String {
-  fatalError("Please implement licenseType(numberOfWheels:) function")
+  if wheels == 18 {
+    return "You will need a commercial trucking license for your vehicle"
+  } else if wheels == 4 || wheels == 6 {
+    return "You will need an automobile license for your vehicle"
+  } else if wheels == 2 || wheels == 3 {
+    return "You will need a motorcycle license for your vehicle"
+  } else {
+    return "We do not issue licenses for those types of vehicles"
+  }
 }
 
 func calculateResellPrice(originalPrice: Int, yearsOld: Int) -> Int {
-  fatalError("Please implement calculateResellPrice(originalPrice:yearsOld:) function")
+  if yearsOld < 3 {
+    return Int(Double(originalPrice) * 0.8)
+  } else if yearsOld >= 3 && yearsOld < 10 {
+    return Int(Double(originalPrice) * 0.7)
+  } else {
+    return Int(Double(originalPrice) * 0.5)
+  }
 }
