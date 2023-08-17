b65cabe3de3191b003e8bbeae2c14b7f43e25eb8
Kim Hallberg <hallberg.kim@gmail.com>
2023-08-17 22:14:42 +0200

    Add `Vexing Vehicle Purchase` exercise

---
+++ vehicle-purchase/Sources/VehiclePurchase/VehiclePurchase.swift
@@ -0,0 +1,11 @@
+func canIBuy(vehicle: String, price: Double, monthlyBudget: Double) -> String {
+  fatalError("Please implement the canIBuy(vehicle:price:monthlyBudget:) function")
+}
+
+func licenseType(numberOfWheels wheels: Int) -> String {
+  fatalError("Please implement licenseType(numberOfWheels:) function")
+}
+
+func calculateResellPrice(originalPrice: Int, yearsOld: Int) -> Int {
+  fatalError("Please implement calculateResellPrice(originalPrice:yearsOld:) function")
+}
