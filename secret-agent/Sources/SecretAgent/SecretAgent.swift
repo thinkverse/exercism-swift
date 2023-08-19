97451f40ed0b7ffad54f52453e684e125bdff882
Kim Hallberg <hallberg.kim@gmail.com>
2023-08-19 03:04:23 +0200

    Update `Secret Agent Double-Null0111` use anonymous closure

--- secret-agent/Sources/SecretAgent/SecretAgent.swift
+++ secret-agent/Sources/SecretAgent/SecretAgent.swift
@@ -1,19 +1,17 @@
 func protectSecret(_ secret: String, withPassword password: String) -> (String) -> String {
-  func printSecret(_ confirmPassword: String) -> String {
+  return { (_ confirmPassword: String) -> String in 
     if password == confirmPassword {
       return secret
     }
 
     return "Sorry. No hidden secrets here."
   }
-
-  return printSecret
 }
 
 func generateCombination(forRoom room: Int, usingFunction f: (Int) -> Int) -> (Int, Int, Int) {
-  let first = f(room)
-  let second = f(first)
-  let third = f(second)
+  let first: Int = f(room)
+  let second: Int = f(first)
+  let third: Int = f(second)
 
   return (first, second, third)
 }
